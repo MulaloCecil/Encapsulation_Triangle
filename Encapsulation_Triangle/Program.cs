2610a5402ecbf64a6b9b622bfeb40355a36e0ee2
MulaloCecil <tshigobamulalo@gmail.com>
2024-06-06 12:30:26 +0200

    Add project files.

---
+++ Encapsulation_Triangle/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            { Console.WriteLine("Enter the base of the triangle: ");
+                double _base = double.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the height of the triangle:");
+                double _height = double.Parse(Console.ReadLine());
+
+                Triangle triangle = new Triangle();
+                triangle.Base = _base;
+                triangle.Height = _height;
+
+                double area = triangle.GetArea();
+                Console.WriteLine($"The area of the triangle is : {area} square units");
+                    }
+            catch (ArgumentException)
+            {
+                Console.WriteLine("Error");
+
+
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input, please enter valid numbers");
+            }
+        }
+    }
+}
---
+++ Encapsulation_Triangle/Triangle.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encapsulation_Triangle
+{
+    internal class Triangle
+    {
+        private double _base;
+        private double _height;
+
+        public double Base
+        {
+            get { return _base; }
+            set
+            {
+                if (value  <= 0)
+                {
+                    throw new ArgumentException("Base must be positive value");
+                }
+                _base = value;
+            }
+        }
+        public double Height
+        {
+            get { return _height; }
+            set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height must be positive value");
+                }
+                _height = value;
+            }
+        }
+
+        public double GetArea()
+        {
+            return 0.5 * _base * _height;
+        }
+    }
+}
